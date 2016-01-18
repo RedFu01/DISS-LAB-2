b74456fbf8080fb439d60623d64af68457eece46
unknown <roedelknoedel>
2016-01-18 11:40:21 +0100

    no message

--- de/tuhh/diss/plotbot/Plotbot.java
+++ de/tuhh/diss/plotbot/Plotbot.java
@@ -4,8 +4,24 @@ import lejos.nxt.Button;
 import lejos.nxt.LCD;
 
 public class Plotbot {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
+		
+		int size = 0;
+		String shape = "";
+		
+		Interface.startUp();
+		while (size == 0){
+			while (shape == ""){
+				shape = Interface.selectShape();
+			}
+			size = Interface.selectSize();
+		}
+		
+		
+		
+		
+		
+		
 		// Some example code to check if the build process works
 		LCD.drawString("Compiled successfully", 0, 0);
 		LCD.drawString("Good Luck!", 0, 1);
