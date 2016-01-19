c91a4e63577a3a3f18c60435a5c2c3e4bcafe7a0
Konrad Fuger <konradfuger@googlemail.com>
2016-01-19 14:13:42 +0100

    adjust the wheels

--- de/tuhh/diss/plotbot/RobotWheels.java
+++ de/tuhh/diss/plotbot/RobotWheels.java
@@ -4,16 +4,22 @@ import lejos.nxt.Motor;
 
 public class RobotWheels {
 	private int distance =0;
-	private final int wheelRadius = 30;
+	private double wheelCircumfence = 28*2*Math.PI;
+	private int gearRatio = 5;
+	
+	public RobotWheels(int gearRatio, int wheelRadius ){
+		this.gearRatio = gearRatio;
+		this.wheelCircumfence =  2* Math.PI * wheelRadius;
+	}
 	
 	void drive(int distance){
-		int deg = distance/this.wheelRadius;
+		int deg = (int)(gearRatio*distance/this.wheelCircumfence);
 		Motor.C.rotate(deg);
 		this.distance += distance;
 	}
 	void driveToDistance(int distance){
 		int delta = this.distance - distance;
-		int deg = distance/this.wheelRadius;
+		int deg = (int)(gearRatio*delta/this.wheelCircumfence);
 		Motor.C.rotate(deg);
 		this.distance = distance;
 	}
