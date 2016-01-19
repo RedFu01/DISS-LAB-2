a8c7c0681c35b48111681cb35624eed065971ec0
Konrad Fuger <konradfuger@googlemail.com>
2016-01-19 11:47:33 +0100

    add wheel implementation

--- de/tuhh/diss/plotbot/RobotWheels.java
+++ de/tuhh/diss/plotbot/RobotWheels.java
@@ -1,5 +1,21 @@
 package de.tuhh.diss.plotbot;
 
+import lejos.nxt.Motor;
+
 public class RobotWheels {
+	private int distance =0;
+	private final int wheelRadius = 30;
+	
+	void drive(int distance){
+		int deg = distance/this.wheelRadius;
+		Motor.C.rotate(deg);
+		this.distance += distance;
+	}
+	void driveToDistance(int distance){
+		int delta = this.distance - distance;
+		int deg = distance/this.wheelRadius;
+		Motor.C.rotate(deg);
+		this.distance = distance;
+	}
 
 }
