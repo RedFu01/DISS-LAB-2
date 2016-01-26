e8dd0448c48d09f0d05b879399941ab333e0f74d
Christopher <christopher.alexander1992@gmail.com>
2016-01-26 01:53:38 +0100

    Removed unused methods in Transform. Fixes in Plotsquare/TUHH regarding initial position (previously differ with calibration). Fixes in Plotline, 3 algorithms (finger crossed).

--- de/tuhh/diss/plotbot/Plotline.java
+++ de/tuhh/diss/plotbot/Plotline.java
@@ -9,86 +9,58 @@ public class Plotline {
 	public static final int speedMotorC = 450; // Default speed
 
 	public void lineInY(int lineLengthY) {
-		int distanceY = (int) Transform.distanceTachoC(lineLengthY);
 
-		Motor.C.setSpeed(speedMotorC);
-		Motor.C.resetTachoCount();
-		
+		Plotbot.wheelMotor.setSpeed(speedMotorC);
 		Plotbot.pen.down();
-		Plotbot.robotWheels.driveToDistance(distanceY);
+		Plotbot.robotWheels.driveToDistance(lineLengthY);
 		Plotbot.pen.up();
 
 	}
 
-	public void lineInX(int lineLengthX) {
+/*	public void lineInX(int lineLengthX) {
 		double angledb;
 		int omegaC;
-		double distanceY;
+		int distanceY;
 
 		angledb = Transform.sweepAngle(lineLengthX); // angle in 'double' type
-		
-		distanceY = Transform.tachoC(Math.abs(angledb));
-		
-		omegaC = (int) Transform.omegaMotorC(angledb, Motor.A.getSpeed());
-		
-		// Cannot give exact angle movement due to double vs. int
 		int angle = (int) Math.round(angledb); // angle in 'int' type
 		
-		int countAngle;
+		distanceY = (int) Math.round(Transform.offsetYdueToX(Math.abs(angledb)));
+		
+		omegaC = (int) Math.round(Transform.omegaMotorC(angledb, Plotbot.armMotor.getSpeed()));
 
 		// Motors' speed setup, synchronized C depends on A
-		Motor.C.setSpeed(omegaC);
+		Plotbot.wheelMotor.setSpeed(omegaC);
 		
 		// Right to left drawing or left to right depends on +/- length of X
-		
-		Plotbot.robotArm.calibrateArm();
+		// +X -> +angle -> right to left, vice versa
+		Plotbot.robotArm.moveTo(0);
 		Plotbot.robotArm.moveTo(angle);
 		Plotbot.pen.down();
 		
 		// 1st half of the line in x-dir
 		Plotbot.robotArm.move(-angle,true);
-		Plotbot.robotWheels.drive((int)-distanceY);
+		Plotbot.robotWheels.drive(-distanceY);
 		
 		// 2nd half of the line in x-dir
 		Plotbot.robotArm.move(-angle, true);
-		Plotbot.robotWheels.drive((int)distanceY);
+		Plotbot.robotWheels.drive(distanceY);
 					
 		Plotbot.pen.up();
-	}
+	}*/
 
-	// Create objects
+/* ------------------------------------------------ ALTERNATIVE 2 ------------------------------------------------ */	
 
-	/*public static final int speedMotorC = 450; // Default speed
-
-	public void lineInY(int lineLengthY) {
-		int lengthY = lineLengthY;
-
-		Plotbot.robotWheels.setSpeed(speedMotorC);
-		Plotbot.wheelMotor.resetTachoCount();
-		
-		Plotbot.pen.down();
-		Plotbot.robotWheels.driveToDistance(lengthY);
-
-		while (Plotbot.wheelMotor.isMoving() && !Button.ESCAPE.isDown()) {
-			LCD.drawInt((int) Transform.distanceC(Plotbot.wheelMotor.getTachoCount()), 0, 0);
-		}
-		Plotbot.wheelMotor.resetTachoCount();
-		
-		Plotbot.pen.up();
-
-	}
-
-	public void lineInX(int lineLengthX) {
+/*	public void lineInX(int lineLengthX) {
 		double angledb;
 		int omegaC;
 		int distanceY = 1;
+		int shift = 2;
 
 		angledb = Transform.sweepAngle(lineLengthX); // angle in 'double' type
-		
-		omegaC = (int) Transform.syncA2C(Plotbot.armMotor.getSpeed());
-		
-		// Cannot give exact angle movement due to double vs. int
 		int angle = (int) Math.round(angledb); // angle in 'int' type
+		
+		omegaC = (int) Math.round(Transform.omegaMotorC(angledb, Plotbot.armMotor.getSpeed()));
 		
 		int countAngle;
 
@@ -97,21 +69,21 @@ public class Plotline {
 		
 		// Right to left drawing or left to right depends on +/- length of X
 		
-		Plotbot.robotArm.init();
+		Plotbot.robotArm.moveTo(0); // Straight arm position
 		Plotbot.robotArm.moveTo(angle);
 		Plotbot.pen.down();
 		
 		// Right to Left drawing
-		if (angle < 0) {
+		if (angle > 0) {
 			// Start drawing from right to middle
-			for (countAngle = 0; countAngle > angle; countAngle--) {
-				Plotbot.robotArm.move(1);
+			for (countAngle = 0; countAngle > -angle; countAngle = countAngle - shift) {
+				Plotbot.robotArm.move(-shift);
 				Plotbot.robotWheels.drive(-distanceY);
 			}
 
 			// Start drawing from middle to left
-			for (countAngle = 0; countAngle > angle; countAngle--) {
-				Plotbot.robotArm.move(1);
+			for (countAngle = 0; countAngle > -angle; countAngle = countAngle - shift) {
+				Plotbot.robotArm.move(-shift);
 				Plotbot.robotWheels.drive(distanceY);
 			}
 			
@@ -121,18 +93,74 @@ public class Plotline {
 		// Left to Right drawing
 		else {
 			// Start drawing from left to middle
-			for (countAngle = 0; countAngle < angle; countAngle++) {
-				Plotbot.robotArm.move(-1);
+			for (countAngle = 0; countAngle > -angle; countAngle = countAngle + shift) {
+				Plotbot.robotArm.move(shift);
 				Plotbot.robotWheels.drive(-distanceY);
 			}
 
 			// Start drawing from middle to right
-			for (countAngle = 0; countAngle < angle; countAngle++) {
-				Plotbot.robotArm.move(-1);
+			for (countAngle = 0; countAngle < angle; countAngle = countAngle + shift) {
+				Plotbot.robotArm.move(shift);
 				Plotbot.robotWheels.drive(distanceY);
 			}
 			
 			Plotbot.pen.up();
 		}
 	}*/
+	
+/* ------------------------------------------------ ALTERNATIVE 3 ------------------------------------------------ */	
+
+	public void lineInX(int lineLengthX) {
+		double angledb;
+		float omegaC;
+		int tetha;
+		
+		angledb = Transform.sweepAngle(lineLengthX); // angle in 'double' type
+		int angle = (int) Math.round(angledb); // angle in 'int' type
+
+		
+		// Right to left drawing or left to right depends on +/- length of X
+		
+		Plotbot.robotArm.moveTo(0); // Straight arm position
+		Plotbot.robotArm.moveTo(angle);
+		Plotbot.pen.down();
+		
+		Plotbot.robotArm.move(-2 * angle, true);
+		
+		tetha = angle;
+		// right to left plot
+		if (angle > 0) {
+			while (tetha >= -angle && tetha <= angle) {
+				omegaC = (float) Math.round(Transform.altOmegaMotorC(Math.abs(tetha), Plotbot.armMotor.getSpeed()));
+				Plotbot.wheelMotor.setSpeed(omegaC);
+				if (tetha >= 0) {
+					Plotbot.wheelMotor.backward();	
+				}
+				
+				else {
+					Plotbot.wheelMotor.forward();
+				}
+				tetha--;			
+			}
+		}
+		
+		// left to right plot
+		else {
+			while (tetha <= -angle && tetha >= angle) {
+				omegaC = (float) Math.round(Transform.altOmegaMotorC(Math.abs(tetha), Plotbot.armMotor.getSpeed()));
+				Plotbot.wheelMotor.setSpeed(omegaC);
+				if (tetha <= 0) {
+					Plotbot.wheelMotor.backward();	
+				}
+				
+				else {
+					Plotbot.wheelMotor.forward();
+				}
+				tetha++;			
+			}	
+		}
+		
+		Plotbot.wheelMotor.stop();
+		Plotbot.pen.up();
+	}
 }
